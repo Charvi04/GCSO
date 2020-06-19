757bb5219634cd316454e2d88f960ccff8b8b297
Charvi04 <66305846+Charvi04@users.noreply.github.com>
2020-06-19 11:19:07 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,18 +1,20 @@
-var car, wall;
-var weight, speed;
+var thickness, wall;
+var bullet, weight, speed;
 
 function setup() {
-  createCanvas(1500,400);
+  createCanvas(1600,400);
   
-  car = createSprite(50, 200, 50, 50);
-  car.shapeColor = "white";
-  car.velocityX = 15;
+  bullet = createSprite(50, 200, 50, 40);
+  bullet.shapeColor = "white";
+  bullet.velocityX = 15;
 
-  wall = createSprite(1500, 200, 60, height/2);
+  wall = createSprite(1200, 200, thickness, height/2);
   wall.shapeColor = color(80,80,80);
 
-  speed = random(55,90);
-  weight = random(400,1500);
+  speed = random(223, 321);
+  weight = random(30, 52);
+
+  thickness = random(22, 83);
 
 }
 
@@ -20,22 +22,50 @@ function setup() {
 function draw() {
   background(0,0,0);  
  
-  if (wall.x-car.x<(car.width+wall.width)/2)
+  if (wall.x-bullet.x<(bullet.width+wall.width)/2)
   {
-    car.velocityX = 0;
+    bullet.velocityX = 0;
     var deformation = 0.5 * weight * speed * speed/22509;
     if(deformation>180)
     {
-      car.shapeColor = color(255,0,0);
+      bullet.shapeColor = color(255,0,0);
     }
     if(deformation<180 && deformation>100)
     {
-      car.shapeColor = color(230,230,0);
+      bullet.shapeColor = color(230,230,0);
     }
     if(deformation<100)
     {
-      car.shapeColor = color(0,255,0);
+      bullet.shapeColor = color(0,255,0);
     }
   }
+  
+  if(hasCollided(bullet, wall))
+  {
+    bullet.velocityX = 0;
+    var damage=0.5 * weight * speed * speed/(thickness *thickness *thickness);
+
+    if(damage>10)
+    {
+      wall.shapeColor = color(255,0,0);
+    }
+
+    if(damage<10)
+    {
+      wall.shapeColor = color(0,255,0);
+    }
+  }
+
   drawSprites();
+}
+
+function hasCollided(lbullet, lwall){
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+
+  if(bulletRightEdge>=wallLeftEdge)
+  {
+    return true
+  }
+    return false;
 }
